8f69e0b966b53e7c9cab9827e3257c19fb8a6d1a
Valentin <valen7est@gmail.com>
2020-11-20 18:23:21 +0100

    Practica terminada

--- Objetos/objetos2.js
+++ Objetos/objetos2.js
@@ -1,8 +1,8 @@
 'use strict';
-Array.prototype.avg = () => {
-  let sumaValores = this.reduce((acu, valor) => acu + valor, 0);
-  return sumaValores;
+Array.prototype.avg = function () {
+  return this.reduce((acu, valor) => acu + valor, 0) / this.length;
 };
 
-const a = [10, 5, 5];
+const a = [10, 5, 5, 4, 4, 2, 4, 6, 8];
+console.log('La media de los numeros: ' + a + ' es:');
 console.log(a.avg());
